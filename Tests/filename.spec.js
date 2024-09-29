f701c52b959389167434a51a6956791255f0b8ec
MuhammadAhmed-Developer <muhammadahmedite@gmail.com>
2024-09-29 15:43:09 +0500

    File name extension .spec

---
+++ Tests/filename.spec.js
@@ -0,0 +1,5 @@
+// this is also text file extention
+import SumValue from "../utils/sum"
+test("test sum function", () => {
+    expect(SumValue(1, 2)).toBe(3)
+})
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <HomeComp />
-      This is main pagedd
+      This is main page
       {/* <img src="/favicon.ico" alt="logo" title="logo" /> */}
       <input type="text" placeholder="input testing" name="name" readOnly value={"Ahmed"} />
       <InputComponent />
