9a8736ce32274bf7866f8d12ab93bee917082c91
MuhammadAhmed-Developer <muhammadahmedite@gmail.com>
2024-09-29 20:52:18 +0500

    what's important

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -27,3 +27,23 @@ test("snapshots for home screen", () => {
     const container = render(<Home />)
     expect(container).toMatchSnapshot()
 })
+
+
+// Important points for testing
+//  what things we shout not tests
+// important points
+
+
+// what we should test
+//  1- Testing component rendering
+//  2- UI element that we write
+//  3- Functions which we write
+//  4- API testing
+//  5- event testing
+//  6- props and states
+//  7- UI condition testing | UI state tasting
+
+//  Avoid testing for
+//  1- external UI library Code
+//  2- No need to test deafult function of js and react
+//  3- sometime we should mock function rathen then testing it in details
